1fc6a60c21beb7bfbe18130567d5d5c725eeae55
utkarsha.singh <t-utsing@microsoft.com>
2017-08-21 17:09:07 +0800

    new

--- AzureSearchBot-master/Node/config.js
+++ AzureSearchBot-master/Node/config.js
@@ -1,8 +1,8 @@
 module.exports = function () {
     //process.env variables defined in Azure if deployed to a web app. For testing, place IDs and Keys inline
-    global.searchName = process.env.AZURE_SEARCH_NAME ? process.env.AZURE_SEARCH_NAME : "smrtsearch";
-    global.indexName = process.env.INDEX_NAME ? process.env.AZURE_SEARCH_NAME : "musicianindex";
-    global.searchKey = process.env.INDEX_NAME ? process.env.AZURE_SEARCH_KEY : "F82FB57F9BC78739A4D266FFDDCCB2E1";
+    global.searchName = process.env.AZURE_SEARCH_NAME ? process.env.AZURE_SEARCH_NAME : "";
+    global.indexName = process.env.INDEX_NAME ? process.env.AZURE_SEARCH_NAME : "";
+    global.searchKey = process.env.INDEX_NAME ? process.env.AZURE_SEARCH_KEY : "";
     
     global.queryString = 'https://' + searchName + '.search.windows.net/indexes/' + indexName + '/docs?api-version=2016-09-01&search=' +searchKey;
 
